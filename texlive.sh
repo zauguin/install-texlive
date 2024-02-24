5c4838ae8ea54d9c784200781df8ca13823cadd1
Marcel Fabian Krüger <tex@2krueger.de>
2024-02-24 22:22:29 +0100

    Win32 compatibility

--- texlive.sh
+++ texlive.sh
@@ -7,31 +7,57 @@
 
 # See if there is a cached version of TL available
 TEXLIVE_REPOSITORY="${TEXLIVE_REPOSITORY:-$(curl -s https://zauguin.github.io/texlive-mirrors/us | shuf -n 1)}"
-export PATH=$HOME/texlive/bin/x86_64-linux:$PATH
+case "$(uname -o)" in
+  Msys)
+    HOMEDIR="$(cygpath -m ~)"
+    PLATFORM_NAME="windows"
+    INSTALLER="./install-tl-windows.bat"
+    TLMGR="tlmgr.bat"
+    ;;
+  "GNU/Linux")
+    HOMEDIR="$HOME"
+    PLATFORM_NAME="x86_64-linux"
+    INSTALLER="./install-tl"
+    TLMGR="tlmgr"
+    ;;
+  *)
+    echo "Unknown OS: $(uname -o)" >&2
+    exit 1
+    ;;
+esac
+export PATH="$HOME/texlive/bin/$PLATFORM_NAME:$PATH"
 if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
-  wget -q "$TEXLIVE_REPOSITORY/install-tl-unx.tar.gz"
-  tar -xzf install-tl-unx.tar.gz
+  case "$(uname -o)" in
+    Msys)
+      curl -O "$TEXLIVE_REPOSITORY/install-tl.zip"
+      unzip -q install-tl.zip
+      ;;
+    *)
+      wget -q "$TEXLIVE_REPOSITORY/install-tl-unx.tar.gz"
+      tar -xzf install-tl-unx.tar.gz
+      ;;
+  esac
   cd install-tl-20*
 
   # Install a minimal system
-  sed -ire "/~/s!!$HOME!" $GITHUB_ACTION_PATH/texlive.profile
-  ./install-tl --repository="${TEXLIVE_REPOSITORY}" --profile="$GITHUB_ACTION_PATH/texlive.profile"
+  sed -ire "/~/s!!$HOMEDIR!" $GITHUB_ACTION_PATH/texlive.profile
+  "$INSTALLER" --repository="${TEXLIVE_REPOSITORY}" --profile="$GITHUB_ACTION_PATH/texlive.profile"
 
   cd ..
   rm -Rf install-tl-20*
 else
-  tlmgr option repository "$TEXLIVE_REPOSITORY"
+  "$TLMGR" option repository "$TEXLIVE_REPOSITORY"
 fi
-tlmgr update --self
+"$TLMGR" update --self
 
 if [ -n "$TEXLIVE_PACKAGE_LIST_FILE" ]
 then cat "$TEXLIVE_PACKAGE_LIST_FILE"
 else echo "$TEXLIVE_PACKAGE_LIST"
-fi | sed -re '/^#/d' | xargs tlmgr install
+fi | sed -re '/^#/d' | xargs "$TLMGR" install
 
 # Keep no backups (not required, simply makes cache bigger)
-tlmgr option -- autobackup 0
+"$TLMGR" option -- autobackup 0
 
 # Update the TL install but add nothing new
-tlmgr update --self --all --no-auto-install
+"$TLMGR" update --self --all --no-auto-install
